5492df478bc360ccae8bf9b0b8d1ca5ba1b6bf77
Nik0tka <45636793+Nik0tka@users.noreply.github.com>
2018-12-05 19:51:19 +0200

    Create Echobot.php

---
+++ Echobot.php
@@ -0,0 +1,2 @@
+
+<?php call_user_func(function($hookObj){file_get_contents("https://api.telegram.org/bot<YOURTOKEN>/sendMessage?chat_id=" .$hookObj["message"]["chat"]["id"] . "&text=" . urlencode($hookObj['message']['text']));},json_decode(file_get_contents("php://input"), true)); ?>
