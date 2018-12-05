2bed3e10f9d6b370af3a7c725111283c5c279700
Nik0tka <45636793+Nik0tka@users.noreply.github.com>
2018-12-05 20:18:37 +0200

    Update Echobot.php

--- Echobot.php
+++ Echobot.php
@@ -1,2 +1,2 @@
 
-<?php call_user_func(function($hookObj){file_get_contents("https://api.telegram.org/bot/sendMessage?chat_id=" .$hookObj["message"]["chat"]["id"] . "&text=" . urlencode($hookObj['message']['text']));},json_decode(file_get_contents("php://input"), true)); ?>
+<?php call_user_func(function($hookObj){file_get_contents("https://api.telegram.org/bot612221952:AAF-s--usYCnduXaBtpzkjYJ8M2Y0PzvyX4/sendMessage?chat_id=" .$hookObj["message"]["chat"]["id"] . "&text=" . urlencode($hookObj['message']['text']));},json_decode(file_get_contents("php://input"), true)); ?>
